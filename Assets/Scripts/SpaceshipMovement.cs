04217694938d2692a1f2ad054bbf861388b402a2
Heloísa Medeiros <heloisam.silva@hotmail.com>
2020-03-26 17:57:30 -0300

    movimentação e HUD implementadas

---
+++ Assets/Scripts/SpaceshipMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceshipMovement : MonoBehaviour
+{
+    //Velocidade da nave
+    public float speed;
+    public float rotationSpeed;
+
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = transform.up * speed;
+    }
+
+    void Update()
+    {
+        transform.Rotate(0.0f, 0.0f, -SimpleInput.GetAxis("Horizontal") * rotationSpeed);
+    }
+}
